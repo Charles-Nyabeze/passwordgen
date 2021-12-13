61e7504a44587f207d7ec5a9b5cde46d315300e2
Charles Nyabeze <charlesnnyabeze@gmail.com>
2021-12-12 19:50:19 -0500

    flip/addinlogic

--- script.js
+++ script.js
@@ -1,41 +1,44 @@
-const characterAmountRange = document.getElementById('characterAmountRange')
-const characterAmountNumber = document.getElementById('characterAmountNumber')
-const includeUppercaseElement = document.getElementById('includeUppercase')
-const includeNumbersElement = document.getElementById('includeNumbers')
-const includeSymbolsElement = document.getElementById('includeSymbols')
-const form = document.getElementById('passwordGeneratorForm')
-const passwordDisplay = document.getElementById('passwordDisplay')
+//Connecting ID's to new names
+let characterRange = document.getElementById('characterRange')
+let characterNumber = document.getElementById('characterNumber')
+let useUppercaseElement = document.getElementById('useUppercase')
+let useNumbersElement = document.getElementById('useNumbers')
+let useSymbolsElement = document.getElementById('useSymbols')
+let form = document.getElementById('passwordForm')
+let ShowPassword = document.getElementById('ShowPassword')
+//end
 
-const UPPERCASE_CHAR_CODES = arrayFromLowToHigh(65, 90)
-const LOWERCASE_CHAR_CODES = arrayFromLowToHigh(97, 122)
-const NUMBER_CHAR_CODES = arrayFromLowToHigh(48, 57)
-const SYMBOL_CHAR_CODES = arrayFromLowToHigh(33, 47).concat(
-  arrayFromLowToHigh(58, 64)
-).concat(
-  arrayFromLowToHigh(91, 96)
-).concat(
-  arrayFromLowToHigh(123, 126)
-)
+//Listen for the input for #'s
+characterNumber.addEventListener('input', syncCharacterAmount)
+characterRange.addEventListener('input', syncCharacterAmount)
+//end
 
-characterAmountNumber.addEventListener('input', syncCharacterAmount)
-characterAmountRange.addEventListener('input', syncCharacterAmount)
-
-form.addEventListener('submit', e => {
-  e.preventDefault()
-  const characterAmount = characterAmountNumber.value
-  const includeUppercase = includeUppercaseElement.checked
-  const includeNumbers = includeNumbersElement.checked
-  const includeSymbols = includeSymbolsElement.checked
-  const password = generatePassword(characterAmount, includeUppercase, includeNumbers, includeSymbols)
-  passwordDisplay.innerText = password
+//Listen for submit click then...
+form.addEventListener('submit', e => { e.preventDefault()
+  const characterAmount = characterNumber.value
+  const useUppercase = useUppercaseElement.checked
+  const useNumbers = useNumbersElement.checked
+  const useSymbols = useSymbolsElement.checked
+  const password = generatePassword(characterAmount, useUppercase, useNumbers, useSymbols)
+  ShowPassword.innerText = password
 })
+//end
 
-function generatePassword(characterAmount, includeUppercase, includeNumbers, includeSymbols) {
-  let charCodes = LOWERCASE_CHAR_CODES
-  if (includeUppercase) charCodes = charCodes.concat(UPPERCASE_CHAR_CODES)
-  if (includeSymbols) charCodes = charCodes.concat(SYMBOL_CHAR_CODES)
-  if (includeNumbers) charCodes = charCodes.concat(NUMBER_CHAR_CODES)
+//Set letters, numbers, and symbols to charCodes + arrays
+let UPPERCASE_CHAR_CODES = arrayFromLowToHigh(65, 90)
+let LOWERCASE_CHAR_CODES = arrayFromLowToHigh(97, 122)
+let NUMBER_CHAR_CODES = arrayFromLowToHigh(48, 57)
+let SYMBOL_CHAR_CODES = arrayFromLowToHigh(33, 47).concat(arrayFromLowToHigh(58, 64)).concat(arrayFromLowToHigh(91, 96)).concat(arrayFromLowToHigh(123, 126))
+//end
+
+//Set up function to generate password
+function generatePassword(characterAmount, useUppercase, useNumbers, useSymbols) {
+  var charCodes = LOWERCASE_CHAR_CODES
+  if (useUppercase) charCodes = charCodes.concat(UPPERCASE_CHAR_CODES)
+  if (useSymbols) charCodes = charCodes.concat(SYMBOL_CHAR_CODES)
+  if (useNumbers) charCodes = charCodes.concat(NUMBER_CHAR_CODES)
   
+  //Randomizer for password
   const passwordCharacters = []
   for (let i = 0; i < characterAmount; i++) {
     const characterCode = charCodes[Math.floor(Math.random() * charCodes.length)]
@@ -43,7 +46,9 @@ function generatePassword(characterAmount, includeUppercase, includeNumbers, inc
   }
   return passwordCharacters.join('')
 }
+//end
 
+//Produce Array
 function arrayFromLowToHigh(low, high) {
   const array = []
   for (let i = low; i <= high; i++) {
@@ -51,9 +56,12 @@ function arrayFromLowToHigh(low, high) {
   }
   return array
 }
+//end
 
+//Syncing Range + Number
 function syncCharacterAmount(e) {
   const value = e.target.value
-  characterAmountNumber.value = value
-  characterAmountRange.value = value
+  characterNumber.value = value
+  characterRange.value = value
 }
+//end
